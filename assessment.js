553926e624aff4793e6aa4c3eb7784192e32d212
hirano-shotaro <153052368+hirano-shotaro@users.noreply.github.com>
2023-12-25 17:18:43 +0900

    Add files via upload

--- assessment.js
+++ assessment.js
@@ -13,14 +13,28 @@ assessmentButton.onclick=()=>{
 
   //診断結果表示エリアの作成
   resultDivision.innerText='';//←17章最後の問題がクリアできない(原因は誤字だった)
-  const header = document.createElement('h3');
-  header.innerText='診断結果';
-  resultDivision.appendChild(header);
+
+  //header Divisionの作成
+  const headerDivision = document.createElement('div');
+  headerDivision.setAttribute('class','card-header text-bg-primary');
+  headerDivision.innerText='診断結果';
+
+  //bodyDivisionの作成
+  const bodyDivision=document.createElement('div');
+  bodyDivision.setAttribute('class','card-body');
 
   const paragraph=document.createElement('p');
+  paragraph.setAttribute('class','card-text');
   const result = assessment(userName);
   paragraph.innerText=result;
-  resultDivision.appendChild(paragraph);
+  bodyDivision.appendChild(paragraph);
+
+  //resultDivisionにBootstrapのスタイルを適用する
+  resultDivision.setAttribute('class','card');
+
+  //resultDivisionとbodyDibisionをresultDivisionに差し込む
+  resultDivision.appendChild(headerDivision);
+  resultDivision.appendChild(bodyDivision);
 
   //ツイートエリアの作成
   tweetDivision.innerText='';//ツイートエリアの子要素の削除
